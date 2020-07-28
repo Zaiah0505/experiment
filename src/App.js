44304717dfbdc7890494c2c6eca08a9918b15438
Isaiah Koh <zaiah0505@hotmail.com>
2020-07-28 12:55:04 +0800

    completed sign up page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar />
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <SignIn />
+        </Route>
+        <Route path="/register">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/components/LoginBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+function LoginBox() {
+  return (
+    <Paper elevation={2}>
+      <Typography>
+        Hello World!
+      </Typography>
+    </Paper>
+  )
+}
+export default LoginBox;
